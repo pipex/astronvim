4942d9c5e2bace14732cc00410b01fab3e33aa14
Felipe Lalanne <felipe@balena.io>
2022-05-16 12:47:30 -0400

    Simplify key mappings

--- init.lua
+++ init.lua
@@ -14,77 +14,45 @@ local config = {
 	polish = function()
 		local opts = { noremap = true, silent = true }
 
-		local map = function(mode, shortcut, command)
-			vim.api.nvim_set_keymap(mode, shortcut, command, opts)
-		end
-		local _map = function(shortcut, command)
-			vim.api.nvim_set_keymap("", shortcut, command, {})
-		end
-		local nmap = function(shortcut, command)
-			map("n", shortcut, command)
-		end
-		local vmap = function(shortcut, command)
-			map("v", shortcut, command)
-		end
-		local imap = function(shortcut, command)
-			map("i", shortcut, command)
-		end
-		local xmap = function(shortcut, command)
-			map("x", shortcut, command)
-		end
+		local map = vim.keymap.set
+		local unmap = vim.keymap.del
 		local tmap = function(shortcut, command)
 			map("t", shortcut, command)
 		end
 		local noremap = function(mode, shortcut, command)
-			vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true })
+			map(mode, shortcut, command, { noremap = true })
 		end
 		-- No remap for all modes
 		local _noremap = function(shortcut, command)
-			vim.api.nvim_set_keymap("", shortcut, command, { noremap = true })
-		end
-
-		local unmap = function(mode, shortcut)
-			vim.api.nvim_del_keymap(mode, shortcut)
-		end
-		local nunmap = function(shortcut)
-			unmap("n", shortcut)
-		end
-		local vunmap = function(shortcut)
-			unmap("v", shortcut)
+			map("", shortcut, command, { noremap = true })
 		end
 
 		-- Set key bindings
-		nmap("<C-s>", ":w!<CR>", opts)
+		map("n", "<C-s>", ":w!<CR>", opts)
 
 		--  ; as :
 		noremap("n", ";", ":")
 
-		-- Use Ctrl+C to go back to normal mode
+		-- Use Ctrl+C to go back to normal mode (useful with ipad keyboard which does not have a esc key)
 		_noremap("<C-c>", "<Esc>")
 
 		-- Buffer nav with Tab
 		noremap("n", "<Tab>", ":bn<CR>")
 		noremap("n", "<S-Tab>", ":bp<CR>")
 
-		vim.cmd([[
-		  autocmd! TermMappings
-		]])
-
 		-- terminal mappings
+
+		-- disable some defaults first
+		unmap("t", "<esc>")
+		unmap("t", "jk")
+
+		-- extra mappings for terminal navigaton
 		tmap("<Leader><esc>", "<c-\\><c-n>")
 		tmap("<Esc><esc>", "<c-\\><c-n>:ToggleTerm<CR>")
 		tmap("<c-h>", "<c-\\><c-n><c-w>h")
 		tmap("<c-j>", "<c-\\><c-n><c-w>j")
 		tmap("<c-k>", "<c-\\><c-n><c-w>k")
 		tmap("<c-l>", "<c-\\><c-n><c-w>l")
-
-		-- Set autocommands
-		-- vim.cmd [[
-		--   augroup packer_conf
-		--     autocmd!
-		--     autocmd bufwritepost plugins.lua source <afile> | PackerSync
-		--   augroup end
-		-- ]]
 	end,
 }
 
