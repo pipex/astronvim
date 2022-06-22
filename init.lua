0ec7fdf8d86bd8ab6729bb41f32ef0c36af2779f
Micah Halter <micah@balena.io>
2022-06-22 11:01:14 -0400

    Move to new mappings API and remove unnecessary defaults

--- init.lua
+++ init.lua
@@ -1,63 +1,34 @@
 local config = {
 	colorscheme = "catppuccin",
-	-- On/off virtual diagnostics text
-	diagnostics = {
-		virtual_text = true,
-	},
 
-	options = {
-		opt = {
-			relativenumber = true,
+	mappings = {
+		n = {
+			--  ; as :
+			[";"] = ":",
+			-- Buffer nav with Tab
+			["<Tab>"] = ":bn<CR>",
+			["<S-Tab>"] = ":bp<CR>",
+			-- Redefine these mappings here as they don't take when defined on plugins
+			["<C-h>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>",
+			["<C-j>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>",
+			["<C-k>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>",
+			["<C-l>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>",
+			["<C-\\>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>",
+			["<C-Space>"] = ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>",
+		},
+		[""] = {
+			-- Use Ctrl+C to go back to normal mode (useful with ipad keyboard which does not have a esc key)
+			["<C-c>"] = "<Esc>",
+		},
+		t = {
+			-- disable some defaults first
+			jk = false,
+			["<esc>"] = false,
+			-- extra mappings for terminal navigaton
+			["<Leader><esc>"] = "<c-\\><c-n>",
+			["<Esc><esc>"] = "<c-\\><c-n>:ToggleTerm<CR>",
 		},
 	},
-
-	polish = function()
-		local opts = { noremap = true, silent = true }
-
-		local map = vim.keymap.set
-		local unmap = vim.keymap.del
-		local tmap = function(shortcut, command)
-			map("t", shortcut, command)
-		end
-		local noremap = function(mode, shortcut, command)
-			map(mode, shortcut, command, { noremap = true })
-		end
-		-- No remap for all modes
-		local _noremap = function(shortcut, command)
-			map("", shortcut, command, { noremap = true })
-		end
-
-		-- Set key bindings
-		map("n", "<C-s>", ":w!<CR>", opts)
-
-		--  ; as :
-		noremap("n", ";", ":")
-
-		-- Use Ctrl+C to go back to normal mode (useful with ipad keyboard which does not have a esc key)
-		_noremap("<C-c>", "<Esc>")
-
-		-- Buffer nav with Tab
-		noremap("n", "<Tab>", ":bn<CR>")
-		noremap("n", "<S-Tab>", ":bp<CR>")
-
-		-- terminal mappings
-
-		-- disable some defaults first
-		unmap("t", "<esc>")
-		unmap("t", "jk")
-
-		-- Redefine these mappings here as they don't take when defined on plugins
-		map("n", "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
-		map("n", "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
-		map("n", "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
-		map("n", "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
-		map("n", "<C-\\>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>", opts)
-		map("n", "<C-Space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", opts)
-
-		-- extra mappings for terminal navigaton
-		tmap("<Leader><esc>", "<c-\\><c-n>")
-		tmap("<Esc><esc>", "<c-\\><c-n>:ToggleTerm<CR>")
-	end,
 }
 
 return config
