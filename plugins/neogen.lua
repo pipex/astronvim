63bbe3930d99fae7ba995e89cb4881de0317a33d
Felipe Lalanne <felipe@balena.io>
2022-06-24 14:02:46 -0400

    Update neogen config

--- plugins/neogen.lua
+++ plugins/neogen.lua
@@ -1,15 +1,7 @@
-return {
-	snippet_engine = "luasnip",
-	languages = {
-		typescript = {
-			template = {
-				annotation_convention = "tsdoc",
-			},
-		},
-		typescriptreact = {
-			template = {
-				annotation_convention = "tsdoc",
-			},
-		},
-	},
-}
+local M = {}
+M.snippet_engine = "luasnip"
+M.languages = {}
+M.languages.python = { template = { annotation_convention = "google_docstrings" } }
+M.languages.typescript = { template = { annotation_convention = "tsdoc" } }
+M.languages.typescriptreact = M.languages.typescript
+return M
