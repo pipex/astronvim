6ffcaa43d79a1cdb3894639833f269238e256ecb
pipex <1822826+pipex@users.noreply.github.com>
2022-10-14 15:26:12 -0300

    Add mason config

---
+++ plugins/mason-lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+	automatic_installation = true,
+	ensure_installed = {
+		"clangd",
+		"cmake",
+		"cssls",
+		"html",
+		"marksman",
+		"jsonls",
+		"pyright",
+		"sqls",
+		"sumneko_lua",
+		"texlab",
+		"tsserver",
+		"yamlls",
+	},
+}
---
+++ plugins/mason-null-ls.lua
@@ -0,0 +1,13 @@
+return {
+	automatic_installation = true,
+	ensure_installed = {
+		"gitsigns",
+		"shellcheck",
+		"stylua",
+		"black",
+		"isort",
+		"shfmt",
+		"cue_fmt",
+		"shellcheck",
+	},
+}
