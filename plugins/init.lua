a390d72ed6c02231f96dbc74e03fef67005e1a60
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-12 15:54:32 -0300

    Update oscyank config

--- plugins/init.lua
+++ plugins/init.lua
@@ -161,23 +161,12 @@ return {
 			end
 
 			local function paste()
-				return {
-					vim.fn.split(vim.fn.getreg(""), "\n"),
-					vim.fn.getregtype(""),
-				}
+				return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
 			end
 
-			vim.g.clipboard = {
-				name = "osc52",
-				copy = {
-					["+"] = copy,
-					["*"] = copy,
-				},
-				paste = {
-					["+"] = paste,
-					["*"] = paste,
-				},
-			}
+			vim.g.clipboard =
+				{ name = "osc52", copy = { ["+"] = copy, ["*"] = copy }, paste = { ["+"] = paste, ["*"] = paste } }
+			vim.cmd([[ set clipboard+=unnamedplus ]])
 		end,
 	},
 }
