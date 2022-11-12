8081af958c3874f00acb64e6553d009a72fdcd3c
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-12 18:12:16 -0300

    Add rust_analyzer settings

--- init.lua
+++ init.lua
@@ -3,5 +3,17 @@ return {
 		servers = {
 			"rust_analyzer",
 		},
+		["server-settings"] = {
+			rust_analyzer = {
+				cargo = {
+					buildScripts = {
+						enable = true,
+					},
+				},
+				procMacro = {
+					enable = true,
+				},
+			},
+		},
 	},
 }
