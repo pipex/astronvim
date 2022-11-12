b1af3fd71234475c10fc20adead9bc1a86b498d5
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-12 15:47:39 -0300

    Add vim-oscyank

--- plugins/init.lua
+++ plugins/init.lua
@@ -153,4 +153,31 @@ return {
 		end,
 		requires = "nvim-lua/plenary.nvim",
 	},
+	["ojroques/vim-oscyank"] = {
+		branch = "main",
+		config = function()
+			local function copy(lines, _)
+				vim.fn.OSCYankString(table.concat(lines, "\n"))
+			end
+
+			local function paste()
+				return {
+					vim.fn.split(vim.fn.getreg(""), "\n"),
+					vim.fn.getregtype(""),
+				}
+			end
+
+			vim.g.clipboard = {
+				name = "osc52",
+				copy = {
+					["+"] = copy,
+					["*"] = copy,
+				},
+				paste = {
+					["+"] = paste,
+					["*"] = paste,
+				},
+			}
+		end,
+	},
 }
