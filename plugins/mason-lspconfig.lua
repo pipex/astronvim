98d678e9a56173e843c99fd31102e041e800b727
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-11 21:01:28 -0300

    Remove unnecessary packages from mason config

--- plugins/mason-lspconfig.lua
+++ plugins/mason-lspconfig.lua
@@ -1,16 +1,11 @@
 return {
 	automatic_installation = true,
 	ensure_installed = {
-		"clangd",
-		"cmake",
 		"cssls",
 		"html",
 		"marksman",
 		"jsonls",
-		"pyright",
-		"sqls",
 		"sumneko_lua",
-		"texlab",
 		"tsserver",
 		"yamlls",
 	},
