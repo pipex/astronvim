a7168fd77b8ec631bc057f72fab6da87320cb860
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-05-01 17:10:46 -0400

    Update osc yank plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
-  { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } },
+  { "akinsho/toggleterm.nvim",     opts = { terminal_mappings = false } },
+  { "rcarriga/nvim-notify",        opts = { background_colour = "#000000" } },
 
   {
     "ethanholz/nvim-lastplace",
@@ -15,21 +15,23 @@ return {
 
   { "gpanders/editorconfig.nvim", lazy = false },
 
-  { "vito-c/jq.vim", ft = "jq" },
+  { "vito-c/jq.vim",              ft = "jq" },
 
   {
-    "ojroques/vim-oscyank",
-    lazy = false,
+    "ojroques/nvim-osc52",
     init = function()
-      vim.cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankRegister +"' | endif]]
-      vim.g.oscyank_term = "default"
+      function copy()
+        if vim.v.event.operator == "y" and vim.v.event.regname == "" then require("osc52").copy_register "" end
+      end
+
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
     end,
   },
 
   {
     "dhruvasagar/vim-table-mode",
     cmd = "TableModeToggle",
-    init = function() vim.g.table_mode_corner = "|" end,
+    init = function() vim.g.table_mode_corner = "+" end,
   },
 
   {
