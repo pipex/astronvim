3811dd505b569ec7d94cc85a1de48adb09dc9f8c
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2024-05-03 00:28:12 -0400

    Disable CMP in comment context

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,15 @@ return {
     cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
 
     return require("astrocore").extend_tbl(opts, {
+      enabled = function()
+        local context = require "cmp.config.context"
+        local disabled = false
+        disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
+        disabled = disabled or (vim.fn.reg_recording() ~= "")
+        disabled = disabled or (vim.fn.reg_executing() ~= "")
+        disabled = disabled or context.in_treesitter_capture "comment"
+        return not disabled
+      end,
       mapping = {
         ["<CR>"] = cmp.mapping(function(fallback)
           if copilot.is_visible() then
@@ -52,8 +61,8 @@ return {
       "zbirenbaum/copilot.lua",
       opts = {
         suggestion = {
-          auto_trigger = true,
-          debounce = 1000,
+          auto_trigger = false,
+          debounce = 750,
         },
       },
     },
