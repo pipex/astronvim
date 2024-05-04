588eafedf6ad025be9bffb5ad013319e222a0911
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2024-05-04 00:25:21 -0400

    Remove copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,10 +2,7 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
-    local cmp, copilot = require "cmp", require "copilot.suggestion"
-
-    cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true end)
-    cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
+    local cmp = require "cmp"
 
     return require("astrocore").extend_tbl(opts, {
       enabled = function()
@@ -18,53 +15,17 @@ return {
         return not disabled
       end,
       mapping = {
-        ["<CR>"] = cmp.mapping(function(fallback)
-          if copilot.is_visible() then
-            copilot.accept()
-          else
-            cmp.mapping.confirm { select = false }(fallback)
-          end
-        end),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if copilot.is_visible() then
-            copilot.next()
-          else
-            fallback()
-          end
-        end),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if copilot.is_visible() then
-            copilot.prev()
-          else
-            fallback()
-          end
-        end),
-        ["<C-c>"] = cmp.mapping(function(fallback)
-          if copilot.is_visible() then
-            copilot.dismiss()
-          else
-            fallback()
-          end
-        end),
-        ["<ESC>"] = cmp.mapping(function(fallback)
-          if copilot.is_visible() then
-            copilot.dismiss()
-          else
-            fallback()
-          end
-        end),
+        ["<CR>"] = cmp.mapping(function(fallback) cmp.mapping.confirm { select = true }(fallback) end),
+        ["<Tab>"] = cmp.mapping(function(fallback) cmp.mapping.select_next_item { select = false }(fallback) end),
+        ["<S-Tab>"] = cmp.mapping(function(fallback) cmp.mapping.select_prev_item {}(fallback) end),
+        -- ["<ESC>"] = cmp.mapping(function(fallback)
+        --  if cmp.visible then
+        --    cmp.abort()
+        --  else
+        --    fallback()
+        --  end
+        -- end),
       },
     })
   end,
-  dependencies = {
-    {
-      "zbirenbaum/copilot.lua",
-      opts = {
-        suggestion = {
-          auto_trigger = false,
-          debounce = 750,
-        },
-      },
-    },
-  },
 }
