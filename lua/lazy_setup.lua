2b729619ebf70b66ca393d6095cef258181d85fa
Felipe Lalanne <felipe@balena.io>
2024-08-01 15:53:40 -0400

    Unpin which-key

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,10 +1,6 @@
 ---@type LazyConfig
 require("lazy").setup({
   { "AstroNvim/AstroNvim", version = "^4", import = "astronvim.plugins" },
-  -- Which key is a bit buggy right now, this pins it to the latest working version
-  { "AstroNvim/astrocore", version = "1.6.1" },
-  { "AstroNvim/astrolsp", version = "1.0.5" },
-  { "folke/which-key.nvim", version = "2.1" },
   { import = "community" },
   { import = "plugins" },
 } --[[@as LazySpec]], {
