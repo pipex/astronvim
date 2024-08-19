84b88bece153f89d1396d28440fed2b793e15f9a
Felipe Lalanne <felipe@balena.io>
2024-08-19 16:36:40 -0400

    Remove terminal-mode mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -38,11 +38,11 @@ return {
         -- Use Ctrl+C to go back to normal mode (useful with ipad keyboard which does not have a esc key)
         ["<C-c>"] = "<Esc>",
       },
-      t = {
-        -- extra mappings for terminal navigaton
-        ["<Leader><esc>"] = "<c-\\><c-n>",
-        ["<Esc><esc>"] = "<c-\\><c-n>:ToggleTerm<CR>",
-      },
+      -- t = {
+      --   -- extra mappings for terminal navigaton
+      --   ["<Leader><esc>"] = "<c-\\><c-n>",
+      --   ["<Esc><esc>"] = "<c-\\><c-n>:ToggleTerm<CR>",
+      -- },
     },
   },
 }
