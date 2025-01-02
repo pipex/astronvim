a20a7d153e996839fc0572e9cad31ad1a502ea20
Felipe Lalanne <felipe@balena.io>
2025-01-02 15:09:49 -0300

    Use cue language pack

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.cue" },
   -- NOTE: this may not work for all language servers
   -- https://github.com/AstroNvim/AstroNvim/commit/4a8443bf46f9071f016bb94e1c2367f2c9956bf2
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
