fc1bfb26bcbe1fc1a3d3b54793ba16ebe890baf0
Felipe Lalanne <felipe@balena.io>
2025-02-24 16:39:24 -0300

    Go back to copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,16 @@ local function has_words_before()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
+local function copilot_action(action)
+  local copilot = require "copilot.suggestion"
+  return function()
+    if copilot.is_visible() then
+      copilot[action]()
+      return true -- doesn't run the next command
+    end
+  end
+end
+
 local function is_private_project()
   -- Check package.json for "private": true
   local package_json = vim.fn.findfile("package.json", ".;")
@@ -29,37 +39,53 @@ local function is_private_project()
   return false
 end
 
-if is_private_project() then return {} end
-
 return {
-  "monkoose/neocodeium",
-  event = "VeryLazy",
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup {
+      filetypes = {
+        javascript = true,
+        typescript = function()
+          if is_private_project() then return false end
+          return true
+        end,
+        lua = true,
+        rust = function()
+          if is_private_project() then return false end
+          return true
+        end,
+        ["*"] = false,
+      },
+      server_opts_overrides = {
+        delayCompletions = 1000,
+      },
+    }
+  end,
   specs = {
     {
       "hrsh7th/nvim-cmp",
       opts = function(_, opts)
-        local cmp, neocodeium = require "cmp", require "neocodeium"
+        local cmp, copilot = require "cmp", require "copilot.suggestion"
         local snip_status_ok, luasnip = pcall(require, "luasnip")
         if not snip_status_ok then return end
 
-        neocodeium.setup {
-          filter = function(bufnr)
-            local allowed_filetypes = { "rust", "javascript", "typescript", "lua" }
-            return vim.tbl_contains(allowed_filetypes, vim.api.nvim_get_option_value("filetype", { buf = bufnr }))
-              and not cmp.visible()
-          end,
-          silent = true,
-        }
+        -- disable cmp autocomplete if copilot is enabled
         cmp.setup {
           completion = {
             autocomplete = false,
           },
         }
 
+        -- if cmp menu is open, then hide copilot
+        cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true end)
+        cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
+
+        -- handle <tab> key depending on if copilot/cmp visibility
         if not opts.mapping then opts.mapping = {} end
         opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-          if neocodeium.visible() then
-            neocodeium.accept()
+          if copilot.is_visible() then
+            copilot.accept()
           elseif cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -71,23 +97,18 @@ return {
           end
         end, { "i", "s" })
 
-        opts.mapping["<C-X>"] = cmp.mapping(function(fallback)
-          if neocodeium.visible() then
-            neocodeium.cycle_or_complete()
-          else
-            fallback()
-          end
-        end)
+        opts.mapping["<C-X>"] = cmp.mapping(copilot_action "next")
+        opts.mapping["<C-Z>"] = cmp.mapping(copilot_action "prev")
         opts.mapping["<C-C>"] = cmp.mapping(function(fallback)
-          if neocodeium.visible() then
-            neocodeium.clear()
+          if copilot.is_visible() then
+            copilot.dismiss()
           else
             fallback()
           end
         end)
         opts.mapping["<ESC>"] = cmp.mapping(function(fallback)
-          if neocodeium.visible() then
-            neocodeium.clear()
+          if copilot.is_visible() then
+            copilot.dismiss()
           else
             fallback()
           end
