90fa622564cfd099d8f404babcacd7eeba729ddc
Felipe Lalanne <felipe@balena.io>
2025-03-06 16:58:50 -0300

    Remove lua packages

--- lua/community.lua
+++ lua/community.lua
@@ -3,7 +3,6 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
