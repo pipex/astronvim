6806623cf185c52a51835257300e6c076b699273
Felipe Lalanne <felipe@balena.io>
2025-04-01 18:49:34 -0300

    Configure astrolsp after snacks loading

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,8 +1,5 @@
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
@@ -75,49 +72,5 @@ return {
         },
       },
     },
-    -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        gD = {
-          function() require("snacks").picker.lsp_declarations() end,
-          desc = "Goto Declaration",
-          cond = "textDocument/declaration",
-        },
-        gd = {
-          function() require("snacks").picker.lsp_definitions() end,
-          desc = "Goto Definition",
-          cond = "textDocument/definition",
-        },
-        gI = {
-          function() require("snacks").picker.lsp_implementations() end,
-          desc = "Goto Implementation",
-          cond = "textDocument/implementation",
-        },
-        gy = {
-          function() require("snacks").picker.lsp_type_definitions() end,
-          desc = "Goto Type Definitions",
-          cond = "textDocument/typeDefinition",
-        },
-        ["<Leader>lR"] = {
-          function() require("snacks").picker.lsp_references() end,
-          desc = "Goto Reference",
-          cond = "textDocument/references",
-        },
-        ["<Leader>uY"] = {
-          function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-          desc = "Toggle LSP semantic highlight (buffer)",
-          cond = function(client)
-            return client.supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
-          end,
-        },
-      },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
   },
 }
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,40 @@
+return {
+  "folke/snacks.nvim",
+  specs = {
+    {
+      "AstroNvim/astrolsp",
+      opts = {
+        mappings = {
+          n = {
+            -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
+            gD = {
+              function() require("snacks").picker.lsp_declarations() end,
+              desc = "Goto Declaration",
+              cond = "textDocument/declaration",
+            },
+            gd = {
+              function() require("snacks").picker.lsp_definitions() end,
+              desc = "Goto Definition",
+              cond = "textDocument/definition",
+            },
+            gI = {
+              function() require("snacks").picker.lsp_implementations() end,
+              desc = "Goto Implementation",
+              cond = "textDocument/implementation",
+            },
+            gy = {
+              function() require("snacks").picker.lsp_type_definitions() end,
+              desc = "Goto Type Definitions",
+              cond = "textDocument/typeDefinition",
+            },
+            ["<Leader>lR"] = {
+              function() require("snacks").picker.lsp_references() end,
+              desc = "Goto Reference",
+              cond = "textDocument/references",
+            },
+          },
+        },
+      },
+    },
+  },
+}
