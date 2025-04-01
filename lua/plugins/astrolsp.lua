6d6a6fb0f2b5b6c61e198561f20a0daf19f59621
Felipe Lalanne <felipe@balena.io>
2025-04-01 18:42:16 -0300

    Fix again snacks bindings

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -80,27 +80,27 @@ return {
       n = {
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         gD = {
-          function() require("Snacks").picker.lsp_declarations() end,
+          function() require("snacks").picker.lsp_declarations() end,
           desc = "Goto Declaration",
           cond = "textDocument/declaration",
         },
         gd = {
-          function() require("Snacks").picker.lsp_definitions() end,
+          function() require("snacks").picker.lsp_definitions() end,
           desc = "Goto Definition",
           cond = "textDocument/definition",
         },
         gI = {
-          function() require("Snacks").picker.lsp_implementations() end,
+          function() require("snacks").picker.lsp_implementations() end,
           desc = "Goto Implementation",
           cond = "textDocument/implementation",
         },
         gy = {
-          function() require("Snacks").picker.lsp_type_definitions() end,
+          function() require("snacks").picker.lsp_type_definitions() end,
           desc = "Goto Type Definitions",
           cond = "textDocument/typeDefinition",
         },
         ["<Leader>lR"] = {
-          function() require("Snacks").picker.lsp_references() end,
+          function() require("snacks").picker.lsp_references() end,
           desc = "Goto Reference",
           cond = "textDocument/references",
         },
